157646eb59fcf384b231aa4bafe5eb26fe19b43a
Chris Ware <c.p.ware@hotmail.com>
2025-08-29 10:42:38 -0700

    Added Marquee

--- src/components/HomePage/RecentWorks/RecentWorks.tsx
+++ src/components/HomePage/RecentWorks/RecentWorks.tsx
@@ -1,6 +1,8 @@
 import LayoutWrapper from "@/components/shared/LayoutWrapper";
 import styles from "./RecentWorks.module.css";
 import SectionIntro from "@/components/shared/SectionIntro/SectionIntro";
+import Marquee from "../Marquee/Marquee";
+import Button from "@/components/shared/Button/Button";
 
 export default function RecentWorks() {
   return (
@@ -11,6 +13,12 @@ export default function RecentWorks() {
             <SectionIntro title='Gallery' />
             <h2 className={styles.heading}>Recent Works</h2>
             <p className={styles.copy}>From our portfolio</p>
+          </div>
+          <div className={styles.bottom}>
+            <Marquee />
+          </div>
+          <div className={styles.btnContainer}>
+            <Button href='/' text='Visit our gallery' btnType='salmon' arrow />
           </div>
         </div>
       </LayoutWrapper>
