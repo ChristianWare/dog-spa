8599db7d89af716f01e50bcfd19d5c3642c33043
Chris Ware <c.p.ware@hotmail.com>
2025-08-29 20:19:28 -0700

    saftey and quality done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,9 @@
 import AboutPageIntro from "@/components/AboutPage/AboutPageInro/AboutPageInro";
 import AboutServicesPreview from "@/components/AboutPage/AboutServicesPreview/AboutServicesPreview";
 import Groomers from "@/components/AboutPage/Groomers/Groomers";
+import SafteyAndQuality from "@/components/AboutPage/SafteyAndQuality/SafteyAndQuality";
+import RecentWorks from "@/components/HomePage/RecentWorks/RecentWorks";
+import Footer from "@/components/shared/Footer/Footer";
 
 export default function AboutPage() {
   return (
@@ -8,6 +11,9 @@ export default function AboutPage() {
       <AboutPageIntro />
       <AboutServicesPreview />
       <Groomers />
+      <SafteyAndQuality />
+      <RecentWorks />
+      <Footer />
     </main>
   );
 }
