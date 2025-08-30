67f68d5ba1192d73b2eb9bb1c0d977e0285e2b46
Chris Ware <c.p.ware@hotmail.com>
2025-08-29 19:24:20 -0700

    groomers done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,13 @@
 import AboutPageIntro from "@/components/AboutPage/AboutPageInro/AboutPageInro";
 import AboutServicesPreview from "@/components/AboutPage/AboutServicesPreview/AboutServicesPreview";
+import Groomers from "@/components/AboutPage/Groomers/Groomers";
 
 export default function AboutPage() {
   return (
     <main>
       <AboutPageIntro />
       <AboutServicesPreview />
+      <Groomers />
     </main>
   );
 }
